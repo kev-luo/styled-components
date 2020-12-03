9032e7cf7586eac3c212e7d03cef1f9dfb1f566d
Kevin Luo <kvn.luo@gmail.com>
2020-12-02 23:20:32 -0500

    add geolocate control

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl';
+import React, { useState } from "react";
+import ReactMapGL, { GeolocateControl } from "react-map-gl";
 
 export default function Map() {
   const [viewport, setViewport] = useState({
     latitude: 45.4211,
     longitude: -75.6903,
-    width: '100vw',
-    height: '100vh',
+    width: "100vw",
+    height: "100vh",
     zoom: 10,
-  })
+  });
+
+  const lightMap = "mapbox://styles/kvnluo/cki8ay4gvbbum19pj0ry9bf4r"
+  const darkMap = "mapbox://styles/kvnluo/cki8azb472zvd19obw4ydlvjd"
 
   return (
     <div>
-      <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}/>
+      <ReactMapGL
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle={darkMap}
+        {...viewport}
+        onViewportChange={newViewport => setViewport(newViewport)}
+      >
+        <GeolocateControl positionOptions={{enableHighAccuracy: true}} trackUserLocation={true} />
+      </ReactMapGL>
     </div>
-  )
+  );
 }
